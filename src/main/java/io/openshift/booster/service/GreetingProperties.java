a8f81312813b2f74ca016060e84ee09e1ac581f4
the-old-dev <gskiker@gmail.com>
2018-01-03 13:49:20 +0000

    Greeting modified

--- src/main/java/io/openshift/booster/service/GreetingProperties.java
+++ src/main/java/io/openshift/booster/service/GreetingProperties.java
@@ -23,7 +23,7 @@ import org.springframework.stereotype.Component;
 @ConfigurationProperties("greeting")
 public class GreetingProperties {
 
-    private String message = "Hello, %s!";
+    private String message = "Hello My Friend, %s!";
 
     public String getMessage() {
         return message;
